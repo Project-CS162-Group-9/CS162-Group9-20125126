b9ddaf3b6a2835dada9e9f08435bab417956d500
Duy Bảo <81920812+bdbao@users.noreply.github.com>
2021-04-25 00:25:29 +0700

    Update MyFunction.h

--- Main Project/My tasks/MyFunction.h
+++ Main Project/My tasks/MyFunction.h
@@ -37,7 +37,6 @@ struct newCourse {
 	int semester;
 };
 
-
 void loadStaff(string path, SchoolYear& scYear);
 void loadClass(string path, SchoolYear& scYear);
 void loadSemester(string path, SchoolYear& scYear);
