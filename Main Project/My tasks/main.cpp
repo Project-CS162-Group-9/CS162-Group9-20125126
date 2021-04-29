e0ff7a4d6ec9790144897dddd536fc37c1af4158
Duy Bảo <81920812+bdbao@users.noreply.github.com>
2021-04-29 10:42:18 +0700

    Update main.cpp

--- Main Project/My tasks/main.cpp
+++ Main Project/My tasks/main.cpp
@@ -7,23 +7,23 @@ int main()
 
 	cout << "Your role (0: staff, 1: student): "; int role; cin >> role;
 	if (role == 0) {
-		cout << "Input your work as in Project CS162_requirements (1, 2, 3, 4, 5, 6,...): "; int task; cin >> task;
-		if (task == 1)		
-			t1_createASchoolYear("_staffs.txt", "_classes.txt", "_semesters.txt", "1_outputSchoolYear.txt", scYear);
-		if (task == 2)		
-			t2_createClasses1stYearStudent("_classes.txt", "2_classes1stYearStudent.txt", scYear);
-		if (task == 3)		
-			t3_addNew1stYearStudents("_students.txt", "3_studentsIn1stYearClasses.txt", scYear);
-		if (task == 4) {	
+		cout << "Input your work as in Project CS162_requirements (1, 2, 3, 4, 5, 6,...): "; int t; cin >> t;
+		if ( == 1)		
+			createASchoolYear("_staffs.txt", "_classes.txt", "_semesters.txt", "1_outputSchoolYear.txt", scYear);
+		if (t == 2)		
+			createClasses1stYearStudent("_classes.txt", "2_classes1stYearStudent.txt", scYear);
+		if (t == 3)		
+			addNew1stYearStudents("_students.txt", "3_studentsIn1stYearClasses.txt", scYear);
+		if (t == 4) {	
 			Class cl;
-			t4_importCSVFileStudent1Class("_CSV file.csv", "4_studentsIn1ClassFromCSVfile.txt", cl);
+			importCSVFileStudent1Class("_CSV file.csv", "4_studentsIn1ClassFromCSVfile.txt", cl);
 		}
-		if (task == 5)		
-			t5_addOnly1stYearStudentsToClasses("_students.txt", "5_AddOnly1stYearStudents.txt", scYear);
+		if (t == 5)		
+			addOnly1stYearStudentsToClasses("_students.txt", "5_AddOnly1stYearStudents.txt", scYear);
 		cout << "Your work has been all in Text File already !\n";
-		if (task == 6) {	
+		if (t == 6) {	
 			Semester sem;
-			t6_createASemester_SchoolyearThatSemesterBelongsTo("_createNewSemester.txt", "6_createASemesters_SchoolyearThatSemesterBelongsTo.txt", sem);
+			createASemester_SchoolyearThatSemesterBelongsTo("_createNewSemester.txt", "6_createASemesters_SchoolyearThatSemesterBelongsTo.txt", sem);
 		}
 	}
 	return 0;
